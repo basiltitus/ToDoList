5a1c1f1405432c5f736f16a85555a21e1462a0bb
basil titus <basiltitus88@gmail.com>
2020-04-13 14:37:59 +0530

    hey

---
+++ ap.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const request=require("request");
+const app=express();
+app.set('view engine', 'ejs');
+var items=["Buy Food","Eat Food","Eat More Food"];
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+
+  var options = { weekday: 'long',
+  day: 'numeric' ,
+  month: 'long'
+  };
+var today  = new Date();
+today=today.toLocaleDateString("en-US", options)
+  res.render("list",{day: today,newI:items});
+});
+app.post("/",function(req,res){
+   var item=req.body.newitem;
+   items.push(item);
+  console.log(item);
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT||3000,function(){
+  console.log("Listening to port 3000");
+})
