432075f6ec16f80870c5aa401527a3af96c7ec5e
basil titus <basiltitus88@gmail.com>
2020-04-14 14:10:20 +0530

    done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const request=require("request");
 const app=express();
 app.set('view engine', 'ejs');
 var items=["Buy Food","Eat Food","Eat More Food"];
+var workitems=[];
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
 app.get("/",function(req,res){
@@ -17,11 +18,19 @@ today=today.toLocaleDateString("en-US", options)
   res.render("list",{day: today,newI:items});
 });
 app.post("/",function(req,res){
+  console.log(req.body);
    var item=req.body.newitem;
+   if(req.body.submit=="work"){
+     workitems.push(item);
+     res.redirect("/work");
+   }
    if(req.body.newitem!="")
    {items.push(item);
   console.log(item);}
   res.redirect("/");
+});
+app.get("/work",function(req,res){
+  res.render("list",{day:"work",newI:workitems});
 });
 
 app.listen(process.env.PORT||3000,function(){
